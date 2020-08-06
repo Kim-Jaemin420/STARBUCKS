1e3953c4c12202ab0cf86e57f7ca67d37fcc17c6
kim jaemin <jany240@naver.com>
2020-08-06 11:19:28 +0900

    거의 완성

--- menu-tab.js
+++ menu-tab.js
@@ -1,27 +1,27 @@
 // DOM노드 찾기
 var items = $('.main-menu__item');
 var btn = $('.main-menu__button');
-// var panel = $('.board section');
-// var tab = $('.tab');
+var panel = $('.board section');
+var tab = $('.tab');
 
 // 기능 구현
 $(document).ready(function()
 {
-  btn.hover(function(e){
+  items.hover(function(e){
     e.preventDefault();
     items.removeClass('menu-act');
-    $(this).parent().addClass('menu-act');
+    $(this).addClass('menu-act');
   },function(){
-    $(this). parent().removeClass('menu-act');
+    $(this).removeClass('menu-act');
   });
 });
 
 
-// tab.click(function(e){
-//   e.preventDefault();
-//   panel.removeClass('tab-act')
-//   $(this).parent().parent().addClass('tab-act')
-// });
+tab.click(function(e){
+  e.preventDefault();
+  panel.removeClass('tab-act')
+  $(this).parent().parent().addClass('tab-act')
+});
 
 // 검색 슬라이드
 jQuery( document ).ready( function() {
@@ -43,66 +43,66 @@ $(document).ready(function () {
 
 
 // 슬라이드 구현
-$(".main-menu__button1").mouseenter(function() {
-  $(".sub-group1").stop().slideDown("fast", function() {
+// $(".main-menu__button1").mouseenter(function() {
+//   $(".sub-group1").stop().slideDown("fast", function() {
 
-  });
-});
-$(".main-menu__button1").mouseleave(function() {
-  $(".sub-group1").stop().slideUp("fast", function() {
+//   });
+// });
+// $(".main-menu__button1").mouseleave(function() {
+//   $(".sub-group1").stop().slideUp("fast", function() {
 
-  });
-});
-$(".main-menu__button2").mouseenter(function() {
-  $(".sub-group2").stop().slideDown("fast", function() {
+//   });
+// });
+// $(".main-menu__button2").mouseenter(function() {
+//   $(".sub-group2").stop().slideDown("fast", function() {
 
-  });
-});
-$(".main-menu__button2").mouseleave(function() {
-  $(".sub-group2").stop().slideUp("fast", function() {
+//   });
+// });
+// $(".main-menu__button2").mouseleave(function() {
+//   $(".sub-group2").stop().slideUp("fast", function() {
 
-  });
-});
-$(".main-menu__button3").mouseenter(function() {
-  $(".sub-group3").stop().slideDown("fast", function() {
+//   });
+// });
+// $(".main-menu__button3").mouseenter(function() {
+//   $(".sub-group3").stop().slideDown("fast", function() {
 
-  });
-});
-$(".main-menu__button3").mouseleave(function() {
-  $(".sub-group3").stop().slideUp("fast", function() {
+//   });
+// });
+// $(".main-menu__button3").mouseleave(function() {
+//   $(".sub-group3").stop().slideUp("fast", function() {
 
-  });
-});
-$(".main-menu__button4").mouseenter(function() {
-  $(".sub-group4").stop().slideDown("fast", function() {
+//   });
+// });
+// $(".main-menu__button4").mouseenter(function() {
+//   $(".sub-group4").stop().slideDown("fast", function() {
 
-  });
-});
-$(".main-menu__button4").mouseleave(function() {
-  $(".sub-group4").stop().slideUp("fast", function() {
+//   });
+// });
+// $(".main-menu__button4").mouseleave(function() {
+//   $(".sub-group4").stop().slideUp("fast", function() {
 
-  });
-});
-$(".main-menu__button5").mouseenter(function() {
-  $(".sub-group5").stop().slideDown("fast", function() {
+//   });
+// });
+// $(".main-menu__button5").mouseenter(function() {
+//   $(".sub-group5").stop().slideDown("fast", function() {
 
-  });
-});
-$(".main-menu__button5").mouseleave(function() {
-  $(".sub-group5").stop().slideUp("fast", function() {
+//   });
+// });
+// $(".main-menu__button5").mouseleave(function() {
+//   $(".sub-group5").stop().slideUp("fast", function() {
 
-  });
-});
-$(".main-menu__button6").mouseenter(function() {
-  $(".sub-group6").stop().slideDown("fast", function() {
+//   });
+// });
+// $(".main-menu__button6").mouseenter(function() {
+//   $(".sub-group6").stop().slideDown("fast", function() {
 
-  });
-});
-$(".main-menu__button6").mouseleave(function() {
-  $(".sub-group6").stop().slideUp("fast", function() {
+//   });
+// });
+// $(".main-menu__button6").mouseleave(function() {
+//   $(".sub-group6").stop().slideUp("fast", function() {
 
-  });
-});
+//   });
+// });
 
 $(document).ready(function(){
   $(".md-close").click(function(){
